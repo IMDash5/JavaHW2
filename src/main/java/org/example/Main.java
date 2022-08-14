30d0c9360764899751f6842385ad2daa415554af
Ivan <mmavrin80@gmail.com>
2022-08-15 02:08:14 +0300

    finish

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,48 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        String str = "aaaaabbbbcdddrrr";
+        char[] letters = str.toCharArray();
+        String res = "";
+        int cnt = 0;
+        for (int j = 0; j < letters.length; j++)
+        {
+            for (int i = 0; i < letters.length; i++)
+            {
+                if (res.contains(Character.toString(letters[j])) & j != letters.length - 1)
+                {
+                    j++;
+                    i = 0;
+
+                }
+                else
+                {
+                    if (letters[i] == letters[j])
+                    {
+                        cnt++;
+                    }
+                }
+            }
+            if (j != letters.length - 1)
+            {
+                res = res + letters[j] + cnt;
+                cnt = 0;
+            }
+            else
+            {
+                if (res.contains(Character.toString(letters[j])))
+                {
+                    break;
+                }
+                else
+                {
+                    cnt = 1;
+                    res = res + letters[j] + cnt;
+                }
+            }
+        }
+        System.out.println(res);
     }
 }
